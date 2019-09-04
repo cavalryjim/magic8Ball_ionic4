66d48527f5fa1eb3ae32fd8f964fa74d906aa462
James Davis <james.davisphd@gmail.com>
2019-09-04 05:55:24 -0500

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public cssClass: string;
+  private answers = ['It is certain.', 'It is decidedly so.', 'Without a doubt.',
+             'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.',
+             'Most likely.', 'Outlook good.', 'Yes.',
+             'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.',
+             'Better not tell you now.', 'Cannot predict now.',
+             'Concentrate and ask again.', "Don't count on it.", 'My reply is no.',
+             'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
 
-  constructor() {}
+  constructor(private toastCtrl: ToastController) {}
+
+  giveAnswer() {
+    this.cssClass = "animated shake";
+    let answer = this.answers[Math.floor(Math.random() * this.answers.length)];
+    // this.presentToast(answer);
+
+    var temp = this;
+    setTimeout(function(){
+      temp.presentToast(answer);
+    }, 2000)
+  }
+
+  async presentToast(answer) {
+    const toast = await this.toastCtrl.create({
+      message: answer,
+      duration: 5000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss().then(() => {
+      console.log('toast dismissed');
+      this.cssClass = "";
+    });
+    toast.present();
+  }
 
 }
